6774f937f4898b60a9ee75d3892ac37e381181e3
hashtagp <hashtag.p27@gmail.com>
2025-02-12 13:51:20 +0530

    refactor: clean up unused dependencies and remove unnecessary files

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
